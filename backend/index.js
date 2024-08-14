894b343f1eeec6d3ee77cca36049571f34669e68
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2024-08-15 00:27:19 +0300

    initial commit: backend - add initial implementation of openai client - add initial implementation of express POST data endpoint which parses body and uses the message to get info from chatgpt

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const OpenAIApi = require("openai").default;
+
+if (process.env["OPENAI_API_KEY"] == undefined) {
+    throw new Error("API_KEY is not set");
+}
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+const openai = new OpenAIApi({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.post("/data", async (req, res) => {
+    const { message } = req.body;
+
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            {
+                role: "user",
+                content: message,
+            },
+        ],
+    });
+
+    res.send(completion.choices[0].message);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
